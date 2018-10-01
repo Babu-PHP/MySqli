1268cb01a4c6305fbae6fd5b3b1b9e41f812621b
Babu-PHP <bheema.babi@gmail.com>
2018-10-01 14:49:56 +0530

    Added Muliple File Upload Folder

---
+++ multiple-files-upload/Readme.txt
@@ -0,0 +1,5 @@
+Author: Yogesh singh
+Author URL: http://makitweb.com/
+Author Email: yogesh@makitweb.com
+Tutorial Link: http://makitweb.com/multiple-files-upload-with-angularjs-and-php/
+
---
+++ multiple-files-upload/upload.php
@@ -0,0 +1,23 @@
+<?php
+
+// Location
+$location = 'upload/';
+
+// Count total files
+$countfiles = count($_FILES['file']['name']);
+
+$filename_arr = array(); 
+// Looping all files
+for ( $i = 0;$i < $countfiles;$i++ ){
+
+   	$filename = $_FILES['file']['name'][$i];
+   
+   	// Upload file
+   	move_uploaded_file($_FILES['file']['tmp_name'][$i],$location.$filename);
+    
+  	$filename_arr[] = $filename;
+
+}
+
+$arr = array('name' => $filename_arr);
+echo json_encode($arr);
